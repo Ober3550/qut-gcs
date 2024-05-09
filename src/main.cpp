06737d9575839861b1f29113d1cbbcab7eadd371
Oblivion <ober3550@gmail.com>
2024-05-10 08:53:47 +1000

    Fixed window resizing

--- src/main.cpp
+++ src/main.cpp
@@ -137,6 +137,7 @@ int main() {
     // Handle event loop
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
     glfwPollEvents();
     glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
